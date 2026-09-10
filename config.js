aa2bd836923e098f8af47fd52021ec36e5bc8c5d
crienovastartup-sudo <crienovastartup@gmail.com>
2026-09-10 01:06:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -17,6 +17,6 @@
  * esta chave na hora, e você atualiza o valor aqui embaixo também.
  */
 window.AVISOFT_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwvz0Y1lIDUtuvl_d-Q6xabJMvhG5C5wCtjzlQ1wUZu6aXcYUnrn3NriuNvSmqwLOD9/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxWkXDvoFpPuqSwEyxOZ9W9heTCe2GsUGF7wz0O3o8AEmylAFFj5yse2B6T9lxh4Eg2/exec",
   apiKey: "bb7b1681-ab69-4803-bc1c-58c739e87ce9",
 };
